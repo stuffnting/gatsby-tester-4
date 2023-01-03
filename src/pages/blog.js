6e6d031e1d56a57846e1631cfeb50dc7c025fcfa
stuffnting <richard@stuffnting.com>
2023-01-03 21:32:28 +0000

    Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,30 +1,29 @@
-import * as React from "react";
-import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import Seo from "../components/seo";
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const BlogPage = ({ data }) => {
   return (
-    <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
-    </Layout>
-  );
-};
+    // ...
+  )
+}
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
-`;
+`
 
-export const Head = () => <Seo title="My Blog Posts" />;
+export const Head = () => <Seo title="My Blog Posts" />
 
-export default BlogPage;
+export default BlogPage
