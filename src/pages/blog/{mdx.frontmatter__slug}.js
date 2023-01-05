26dc21d40efc14d123a639b79126e947d9cff009
stuffnting <richard@stuffnting.com>
2023-01-05 17:18:49 +0000

    Part 6

---
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
+
+const BlogPost = ({ data, children }) => {
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      {children}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+    }
+  }
+`;
+
+export const Head = (props) => {
+  const data = props.data;
+  console.log(props);
+  return <Seo title={data.mdx.frontmatter.title} />;
+};
+
+export default BlogPost;
